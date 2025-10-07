178e4e182c3ce1acbcff05f71449b878e808fbbf
Jhosselin24 <jhosselin.naula@epn.edu.ec>
2025-10-06 22:06:52 -0500

    Add files via upload

---
+++ API/datosGif.js
@@ -0,0 +1,13 @@
+const obtenerGif = async ()=> {
+    const request = await fetch ("https://api.giphy.com/v1/gifs/search?api_key=lerx9FDWD5PH78M207Cj95ac4KTt8G3v&q=goku");
+
+    const respuesta = await request.json();
+
+    const gifUrl = respuesta.data[0].images.original.url;
+
+    const img = document.querySelector("#gif");
+
+    img.src = gifUrl;
+}
+
+obtenerGif();
